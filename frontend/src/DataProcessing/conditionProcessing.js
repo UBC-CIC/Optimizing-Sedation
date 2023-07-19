c49e57f29d7116104adb8924a14815f51f9969b8
Liam Driscoll <liamur.driscoll@hotmail.com>
2023-07-19 15:54:58 -0700

    Only display most recent entries for duplicates

--- frontend/src/DataProcessing/conditionProcessing.js
+++ frontend/src/DataProcessing/conditionProcessing.js
@@ -1,33 +1,47 @@
 function processConditionData(ConditionData) {
-    const results = [];
-    const uniqueEntries = new Set();
-  
-    if (ConditionData && ConditionData.entry[0].resource) {
-      ConditionData.entry.forEach((entry) => {
-        const ConditionType =
-          entry.resource.code
-            ? entry.resource.code.text
-            : 'N/A';
-        
-        const ConditionStatus =
-          entry.resource.clinicalStatus
-            ? entry.resource.clinicalStatus.coding[0].code
-            : 'N/A';
+  const results = [];
+  const uniqueEntries = new Map(); // // Keeps track of the most recent entries for each ConditionType
 
-        const ConditionTime =
-          entry.resource.recordedDate
-            ? entry.resource.recordedDate
-            : 'N/A';
-  
-        const entryString = `${ConditionType}-${ConditionStatus}-${ConditionTime}`;
-          if (!uniqueEntries.has(entryString)) {
-            uniqueEntries.add(entryString);
-            results.push({ ConditionType, ConditionStatus, ConditionTime });
-          }
-      });
-    }
-    
-    return results;
+  if (ConditionData && ConditionData.entry[0].resource) {
+    ConditionData.entry.forEach((entry) => {
+      const ConditionType =
+        entry.resource.code
+          ? entry.resource.code.text
+          : 'N/A';
+      
+      const ConditionStatus =
+        entry.resource.clinicalStatus
+          ? entry.resource.clinicalStatus.coding[0].code
+          : 'N/A';
+
+      const ConditionTime =
+        entry.resource.recordedDate
+          ? entry.resource.recordedDate
+          : 'N/A';
+
+      // If we have already seen this ConditionType, compare ConditionTime and update the entry if it's more recent.
+      if (uniqueEntries.has(ConditionType)) {
+        const currentEntry = uniqueEntries.get(ConditionType);
+        if (compareDates(currentEntry.ConditionTime, ConditionTime) > 0) {
+          uniqueEntries.set(ConditionType, { ConditionType, ConditionStatus, ConditionTime });
+        }
+      } else {
+        uniqueEntries.set(ConditionType, { ConditionType, ConditionStatus, ConditionTime });
+      }
+    });
+
+    // Convert the map values back to an array of results
+    results.push(...uniqueEntries.values());
   }
+  
+  return results;
+}
 
-  export default processConditionData;
+// Compares two dates in the format "year-month-day" or "year-month-dayThour:minute:second"
+function compareDates(date1, date2) {
+const timestamp1 = new Date(date1).getTime();
+const timestamp2 = new Date(date2).getTime();
+return timestamp2 - timestamp1;
+}
+
+export default processConditionData;
--- frontend/src/DataProcessing/immunizationProcessing.js
+++ frontend/src/DataProcessing/immunizationProcessing.js
@@ -1,6 +1,6 @@
 function processImmunizationData(ImmunizationData) {
   const results = [];
-  const uniqueEntries = new Set();
+  const uniqueEntries = new Map(); // Keeps track of the most recent entries for each ImmunizationType
 
   if (ImmunizationData && ImmunizationData.entry[0].resource) {
     ImmunizationData.entry.forEach((entry) => {
@@ -12,19 +12,33 @@ function processImmunizationData(ImmunizationData) {
         ? entry.resource.status
         : 'N/A';
 
-      const ImmunizationTime = entry.resource.occurrenceDateTime
+      let ImmunizationTime = entry.resource.occurrenceDateTime
         ? entry.resource.occurrenceDateTime
         : 'N/A';
 
-      const entryString = `${ImmunizationType}-${ImmunizationStatus}-${ImmunizationTime}`;
-      if (!uniqueEntries.has(entryString)) {
-        uniqueEntries.add(entryString);
-        results.push({ ImmunizationType, ImmunizationStatus, ImmunizationTime });
+      // If we have already seen this ImmunizationType, compare ImmunizationTime and update the entry if it's more recent.
+      if (uniqueEntries.has(ImmunizationType)) {
+        const currentEntry = uniqueEntries.get(ImmunizationType);
+        if (compareDates(currentEntry.ImmunizationTime, ImmunizationTime) > 0) {
+          uniqueEntries.set(ImmunizationType, { ImmunizationType, ImmunizationStatus, ImmunizationTime });
+        }
+      } else {
+        uniqueEntries.set(ImmunizationType, { ImmunizationType, ImmunizationStatus, ImmunizationTime });
       }
     });
+
+    // Convert the map values back to an array of results
+    results.push(...uniqueEntries.values());
   }
 
   return results;
+}
+
+// Compares two dates in the format "year-month-day" or "year-month-dayThour:minute:second"
+function compareDates(date1, date2) {
+  const timestamp1 = new Date(date1).getTime();
+  const timestamp2 = new Date(date2).getTime();
+  return timestamp2 - timestamp1;
 }
 
 export default processImmunizationData;
