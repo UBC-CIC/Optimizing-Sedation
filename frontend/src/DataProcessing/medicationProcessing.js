f6606e7c357761f8dda91ef56a745443c8d399a0
Liam Driscoll <liamur.driscoll@hotmail.com>
2023-07-21 16:19:38 -0700

    Revert "Only display most recent entries for duplicates"
    This reverts commit c49e57f29d7116104adb8924a14815f51f9969b8.

--- frontend/src/DataProcessing/medicationProcessing.js
+++ frontend/src/DataProcessing/medicationProcessing.js
@@ -1,6 +1,6 @@
 function processMedicationData(MedicationData) {
     const results = [];
-    const uniqueEntries = new Map(); // Keeps track of the most recent entries for each MedicationType
+    const uniqueEntries = new Set();
   
     if (MedicationData && MedicationData.entry[0].resource) {
       MedicationData.entry.forEach((entry) => {
@@ -18,30 +18,16 @@ function processMedicationData(MedicationData) {
           entry.resource.authoredOn
             ? entry.resource.authoredOn
             : 'N/A';
-
-          // If we have already seen this MedicationType, compare MedicationTime and update the entry if it's more recent.
-        if (uniqueEntries.has(MedicationType)) {
-          const currentEntry = uniqueEntries.get(MedicationType);
-          if (compareDates(currentEntry.MedicationTime, MedicationTime) > 0) {
-            uniqueEntries.set(MedicationType, { MedicationType, MedicationStatus, MedicationTime });
-          }
-        } else {
-          uniqueEntries.set(MedicationType, { MedicationType, MedicationStatus, MedicationTime });
+  
+        const entryString = `${MedicationType}-${MedicationStatus}-${MedicationTime}`;
+        if (!uniqueEntries.has(entryString)) {
+          uniqueEntries.add(entryString);
+          results.push({ MedicationType, MedicationStatus, MedicationTime });
         }
       });
-
-      // Convert the map values back to an array of results
-      results.push(...uniqueEntries.values());
     }
     
     return results;
-  }
-
-  // Compares two dates in the format "year-month-day" or "year-month-dayThour:minute:second"
-function compareDates(date1, date2) {
-  const timestamp1 = new Date(date1).getTime();
-  const timestamp2 = new Date(date2).getTime();
-  return timestamp2 - timestamp1;
   }
 
 export default processMedicationData;
