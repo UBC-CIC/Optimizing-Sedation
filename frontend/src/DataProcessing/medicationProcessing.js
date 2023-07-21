52086e61f2c4d6bb4abffb63c708e247f4068698
Liam Driscoll <liamur.driscoll@hotmail.com>
2023-07-21 16:50:54 -0700

    Only list medications from last six months

--- frontend/src/DataProcessing/medicationProcessing.js
+++ frontend/src/DataProcessing/medicationProcessing.js
@@ -1,33 +1,46 @@
 function processMedicationData(MedicationData) {
-    const results = [];
-    const uniqueEntries = new Set();
-  
-    if (MedicationData && MedicationData.entry[0].resource) {
-      MedicationData.entry.forEach((entry) => {
-        const MedicationType =
-          entry.resource.medicationCodeableConcept
-            ? entry.resource.medicationCodeableConcept.text
-            : 'N/A';
-        
-        const MedicationStatus =
-          entry.resource.status
-            ? entry.resource.status
-            : 'N/A';
+  const results = [];
+  const uniqueEntries = new Set();
 
-        const MedicationTime =
-          entry.resource.authoredOn
-            ? entry.resource.authoredOn
-            : 'N/A';
-  
-        const entryString = `${MedicationType}-${MedicationStatus}-${MedicationTime}`;
-        if (!uniqueEntries.has(entryString)) {
-          uniqueEntries.add(entryString);
+  if (MedicationData && MedicationData.entry[0].resource) {
+    MedicationData.entry.forEach((entry) => {
+      const MedicationType =
+        entry.resource.medicationCodeableConcept
+          ? entry.resource.medicationCodeableConcept.text
+          : 'N/A';
+
+      const MedicationStatus =
+        entry.resource.status
+          ? entry.resource.status
+          : 'N/A';
+
+      const MedicationTime =
+        entry.resource.authoredOn
+          ? entry.resource.authoredOn
+          : 'N/A';
+
+      const entryString = `${MedicationType}-${MedicationStatus}-${MedicationTime}`;
+      if (!uniqueEntries.has(entryString)) {
+        uniqueEntries.add(entryString);
+
+        // Checks if MedicationTime is within the last six months
+        if (isWithinLastSixMonths(MedicationTime)) {
           results.push({ MedicationType, MedicationStatus, MedicationTime });
         }
-      });
-    }
-    
-    return results;
+      }
+    });
   }
+
+  return results;
+}
+
+// Checks if a date string is within the last six months
+function isWithinLastSixMonths(dateString) {
+  const sixMonthsAgo = new Date();
+  sixMonthsAgo.setMonth(sixMonthsAgo.getMonth() - 6);
+
+  const medicationDate = new Date(dateString);
+  return medicationDate >= sixMonthsAgo;
+}
 
 export default processMedicationData;
