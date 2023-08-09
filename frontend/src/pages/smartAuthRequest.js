fe94320d4e0cfbd4a6dd69ad8cb6b61d9d35957e
vsaosuo <visal.saosuo@gmail.com>
2023-08-09 09:50:01 -0700

    Set absolute redirect uri and clientSecret for private EHR

--- frontend/src/pages/smartAuthRequest.js
+++ frontend/src/pages/smartAuthRequest.js
@@ -4,7 +4,8 @@ import FHIR from 'fhirclient';
 export default function SmartAuth(){
 
     FHIR.oauth2.authorize({
-        'redirectUri': process.env.REACT_APP_REDIRECT_URI, 
+        'redirectUri': '/',
+        'clientSecret': process.env.REACT_APP_CLIENT_SECRET,
         'client_id': process.env.REACT_APP_CLIENT_ID,
         'scope':  'patient/Patient.read patient/Observation.read patient/DiagnosticReport.read patient/Immunization.read patient/MedicationRequest.read launch online_access openid profile',
     });
