e47e08b9363e60ef920cf5973a303c3e927dd6a7
vsaosuo <visal.saosuo@gmail.com>
2023-08-14 17:02:55 -0700

    change ecr stack name

--- backend/cdk/lib/cdk-ecr.ts
+++ backend/cdk/lib/cdk-ecr.ts
@@ -3,18 +3,14 @@ import { Construct } from "constructs";
 import * as ecr from 'aws-cdk-lib/aws-ecr';
 import * as assets from 'aws-cdk-lib/aws-ecr-assets';
 
-// Create a ECR props interface
-export interface EcrProps extends StackProps {
-    org: string,
-    environment: string,
-}
-
 export class EcrStack extends Stack {
-    constructor(scope: Construct, id: string, props: EcrProps) {
+    public readonly repo: ecr.Repository;
+
+    constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
         // Create an ECR repository
-        const repo = new ecr.Repository(this, `${props.org}-${props.environment}-ecr`, {
+        this.repo = new ecr.Repository(this, `repository`, {
             repositoryName: `docker-repo`
         });
     }
