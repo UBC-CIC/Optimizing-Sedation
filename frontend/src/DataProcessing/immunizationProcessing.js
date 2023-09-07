f73ab33f4cbeac316d96a61dcb579ade07b8d33d
vsaosuo <visal.saosuo@gmail.com>
2023-09-07 11:54:23 -0700

    add trycatch to handle error

--- frontend/src/DataProcessing/immunizationProcessing.js
+++ frontend/src/DataProcessing/immunizationProcessing.js
@@ -4,23 +4,25 @@ function processImmunizationData(ImmunizationData) {
 
   if (ImmunizationData && ImmunizationData.entry) {
     ImmunizationData.entry.forEach((entry) => {
-      const ImmunizationType = entry.resource
-        ? entry.resource.vaccineCode.text
-        : 'N/A';
+      try {
+        const ImmunizationType = entry.resource
+          ? entry.resource.vaccineCode.text
+          : 'N/A';
 
-      const ImmunizationStatus = entry.resource.status
-        ? entry.resource.status
-        : 'N/A';
+        const ImmunizationStatus = entry.resource.status
+          ? entry.resource.status
+          : 'N/A';
 
-      const ImmunizationTime = entry.resource.occurrenceDateTime
-        ? entry.resource.occurrenceDateTime.substring(0, 10)
-        : 'N/A';
+        const ImmunizationTime = entry.resource.occurrenceDateTime
+          ? entry.resource.occurrenceDateTime.substring(0, 10)
+          : 'N/A';
 
-      const entryString = `${ImmunizationType}-${ImmunizationStatus}-${ImmunizationTime}`;
-      if (!uniqueEntries.has(entryString)) {
-        uniqueEntries.add(entryString);
-        results.push({ ImmunizationType, ImmunizationStatus, ImmunizationTime });
-      }
+        const entryString = `${ImmunizationType}-${ImmunizationStatus}-${ImmunizationTime}`;
+        if (!uniqueEntries.has(entryString)) {
+          uniqueEntries.add(entryString);
+          results.push({ ImmunizationType, ImmunizationStatus, ImmunizationTime });
+        }
+      } catch (error) {}
     });
   }
 
--- frontend/src/DataProcessing/medicationProcessing.js
+++ frontend/src/DataProcessing/medicationProcessing.js
@@ -4,30 +4,32 @@ function processMedicationData(MedicationData) {
 
   if (MedicationData && MedicationData.entry) {
     MedicationData.entry.forEach((entry) => {
-      const MedicationType =
-        entry.resource.medicationCodeableConcept
-          ? entry.resource.medicationCodeableConcept.text
-          : 'N/A';
-
-      const MedicationStatus =
-        entry.resource.status
-          ? entry.resource.status
-          : 'N/A';
-
-      const MedicationTime =
-        entry.resource.authoredOn
-          ? entry.resource.authoredOn.substring(0, 10)
-          : 'N/A';
-
-      const entryString = `${MedicationType}-${MedicationStatus}-${MedicationTime}`;
-      if (!uniqueEntries.has(entryString)) {
-        uniqueEntries.add(entryString);
-
-        // Checks if MedicationTime is within the last six months
-        if (isWithinLastSixMonths(MedicationTime)) {
-          results.push({ MedicationType, MedicationStatus, MedicationTime });
+      try {
+        const MedicationType =
+          entry.resource.medicationCodeableConcept
+            ? entry.resource.medicationCodeableConcept.text
+            : 'N/A';
+  
+        const MedicationStatus =
+          entry.resource.status
+            ? entry.resource.status
+            : 'N/A';
+  
+        const MedicationTime =
+          entry.resource.authoredOn
+            ? entry.resource.authoredOn.substring(0, 10)
+            : 'N/A';
+  
+        const entryString = `${MedicationType}-${MedicationStatus}-${MedicationTime}`;
+        if (!uniqueEntries.has(entryString)) {
+          uniqueEntries.add(entryString);
+  
+          // Checks if MedicationTime is within the last six months
+          if (isWithinLastSixMonths(MedicationTime)) {
+            results.push({ MedicationType, MedicationStatus, MedicationTime });
+          }
         }
-      }
+      } catch (error) {}
     });
   }
 
